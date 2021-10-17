ede88bd8f945c3da493031066773302bf9a3b949
AgIgor <agigor@outlook.com>
2021-10-17 12:11:12 -0300

    via linux

---
+++ index.js
@@ -0,0 +1,16 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAYGQq-JV81hYECP73faMCwzgk_Ay0Am-Y",
+  authDomain: "databasefirebasefirestore.firebaseapp.com",
+  databaseURL: "https://databasefirebasefirestore-default-rtdb.firebaseio.com",
+  projectId: "databasefirebasefirestore",
+  storageBucket: "databasefirebasefirestore.appspot.com",
+  messagingSenderId: "407141110375",
+  appId: "1:407141110375:web:1b1ecc00c85aabd2713ac2",
+  measurementId: "G-Y2QDD0H8NF"
+};
+
+  firebase.initializeApp(firebaseConfig)
+
+
+
+console.log("teste")
