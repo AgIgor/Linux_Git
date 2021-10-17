a0d3451f3c2e62a233a9c425a717991940803f7b
AgIgor <agigor@outlook.com>
2021-10-17 12:24:26 -0300

    teste vscode

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
-const firebaseConfig = {
-  apiKey: "AIzaSyAYGQq-JV81hYECP73faMCwzgk_Ay0Am-Y",
-  authDomain: "databasefirebasefirestore.firebaseapp.com",
-  databaseURL: "https://databasefirebasefirestore-default-rtdb.firebaseio.com",
-  projectId: "databasefirebasefirestore",
-  storageBucket: "databasefirebasefirestore.appspot.com",
-  messagingSenderId: "407141110375",
-  appId: "1:407141110375:web:1b1ecc00c85aabd2713ac2",
-  measurementId: "G-Y2QDD0H8NF"
-};
 
-  firebase.initializeApp(firebaseConfig)
+  firebase.initializeApp({
+    apiKey: "AIzaSyAYGQq-JV81hYECP73faMCwzgk_Ay0Am-Y",
+    authDomain: "databasefirebasefirestore.firebaseapp.com",
+    databaseURL: "https://databasefirebasefirestore-default-rtdb.firebaseio.com",
+    projectId: "databasefirebasefirestore",
+    storageBucket: "databasefirebasefirestore.appspot.com",
+    messagingSenderId: "407141110375",
+    appId: "1:407141110375:web:1b1ecc00c85aabd2713ac2",
+    measurementId: "G-Y2QDD0H8NF"
+  });
 
 
-console.log("ok")
+let db = firebase.database();
+let users = db.ref("users");
+
+users.set("ok");
+
+
